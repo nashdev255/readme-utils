bde80e139cc629fc26843b3024f8cceff223575a
nashdev255 <yutori1107@gmail.com>
2023-09-19 22:34:31 +0900

    add comments

--- passedTime.js
+++ passedTime.js
@@ -1,11 +1,25 @@
+/**
+ * @file passedTime
+ * @author nashdev255
+ * @brief get difference between two dates
+ */
+
+/**
+ * @name DATE_I_STARTED_PROGRIMMING
+ * Format : yyyy/mm/dd
+ */
 const DATE_I_STARTED_PROGRIMMING = '2022/04/08';
 
+const getToday = () => {
+  return new Date(
+    new Date().getFullYear() + '/' +
+    (new Date().getMonth() + 1) + '/' +
+    new Date().getDate()
+  );
+}
+
 const dateIstartedPrograminng = new Date(DATE_I_STARTED_PROGRIMMING);
-const today = new Date(
-  new Date().getFullYear() + '/' +
-  new Date().getMonth() + '/' +
-  new Date().getDay()
-);
+const 
 
 const passedMilliSecond = today.getTime() - dateIstartedPrograminng.getTime();
 const passedSecond = passedMilliSecond / 1000;
@@ -15,5 +29,3 @@ const passedDay    = passedHour / 24;
 
 const passedWeek   = passedDay / 7;
 const passedYear   = passedDay / 365;
-
-console.log(passedYear);
